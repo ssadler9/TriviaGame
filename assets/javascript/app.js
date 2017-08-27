14b5d37374cddc84a328c3fbe85c73c1d9eb256b
Sam Sadler <samsadler9@sams-air.attlocal.net>
2017-08-26 19:50:45 -0500

    css styling, basic jquery, much more to do

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,96 +1,144 @@
-// 10 questions for user to answer
-var questions = {
-    question1: "Which player was not part of the Dallas Cowboys Triplets?",
-	question2: "When was the last time the Dallas Cowboys won The Superbowl?",
-    question3: "Where did Dak Prescott attend college?",
-    question4: "Roger Staubach played which position?",
-    question5: "What record did Emmitt Smith set?",
-    question6: "Which team is the Dallas Cowboys mosted hated rival?",
-    question7: "Before the NFC/AFC Split, which team shared Dallas, TX?",
-    question8: "What franchise is the most valued franchise in all of sports?",
-    question9: "What position did Jay Novacek play?",
-    question10: "When will the Dallas Cowboys when their next Superbowl?",
-};
-console.log(questions.question6);
-// answer bank
-var answers1={ 
-			    a:"Emmitt Smith", 
-				b:"Troy Aikman", 
-				c:"Tony Dorsett", 
-				d:"Michael Irvin",
-			};
-			console.log(answers1);
-var answers2={ 
-				a:"1999",
-				b:"1996",
-				c:"1994",
-				d:"1997",
-			};
-			console.log(answers2);
-var answers3={
-				a:"Mississippi State University",
-				b:"Purdue",
-				c:"LSU",
-				d:"USC",
-			};
-			console.log(answers3);
-var answers4={ 
-				a:"Running Back",
-				b:"Wide Receiver",
-				c:"Left-out",
-				d:"Quarterback",
-			};
-			console.log(answers4);
-var answers5={ 
-				a:"Career Rushing Yards",
-				b:"Most Fumbles",
-				c:"Fastest 40",
-				d:"Most Rushing Attempts",
-			};
-			console.log(answers5);
-var answers6={ 
-				a:"Ney York Giants",
-				b:"Washington Redskins",
-				c:"Philadelphia Eagles",
-				d:"All of the Above",
-			};
-			console.log(answers6);
-var answers7={ 
-				a:"Kansas City Chiefs",
-				b:"Tennessee Titans", 
-				c:"New York Giants", 
-				d:"Arizona Cardinals",
-			};
-			console.log(answers7);
-var answers8={
-				a:"New York Yankees",
-				b:"Miami Heat",
-				c:"Dallas Cowboys",
-				d:"Oakland Raiders",
-			};
-			console.log(answers8);
-var answers9={ 
-				a:"Quarterback",
-				b:"Tight End",
-				c:"Center",
-				d:"Middle-Linebacker"
-			};
-			console.log(answers9);
-var answers10={
-				a:"2017",
-				b:"2018",
-				c:"2060",
-				d:"Never",
-			};
-			console.log(answers10);
-
-// answer bank of correct answers
-// answer bank of incorrect answers
-// timer for the game -- 45 seconds
-// verify if the answer is correct
-
 // increment correct answers
 var correctAnswers = 0;
 // increment incorrect answers
 var incorrectAnswers = 0;
+// 10 questions for user to answer
+var questions = [{
+        question: "Which player was not part of the Dallas Cowboys Triplets?",
+        choices: ["Emmitt Smith", "Troy Aikman", "Tony Dorsett", "Michael Irvin"]
+        // correct: 2,
+    },
+    {
+        question: "When was the last time the Dallas Cowboys won The Superbowl?",
+        choices: ["1999", "1996", "1994", "1997"],
+        // correct: 1,
+    },
+    {
+        question: "Where did Dak Prescott attend college?",
+        choices: ["Mississippi State", "Purdue", "LSU", "USC"],
+        // correct: 0
+    },
+    {
+        question: "Roger Staubach played which position?",
+        choices: ["Running Back", "Wide Receiver", "Left Out", "Quarterback"],
+        // correct: 3
+    },
+    {
+        question: "What record did Emmitt Smith set?",
+        choices: ["Career Rushing Yards", "Most Fumbles", "Fastest 40", "Most Rushing Attempts"],
+        // correct: 0
+    },
+    {
+        question: "Which team is the Dallas Cowboys mosted hated rival?",
+        choices: ["New York Giants", "Washington Redskins", "Philadelphia Eagles", "All of the Above"],
+        // correct: 3
+    },
+    {
+        question: "Before the NFC/AFC Split, which team shared Dallas, TX?",
+        choices: ["Tennessee Titans", "Arizona Cardinals", "Kansas City Chiefs", "Houston Oilers"],
+        // correct: 2
+    },
+    {
+        question: "What franchise is the most valued franchise in all of sports?",
+        choices: ["New York Yankees", "Dallas Cowboys", "Miami Heat", "Oakland Raiders"],
+        // correct: 1
+    },
+    {
+        question: "What position did Jay Novacek play?",
+        choices: ["Quarterback", "Center", "Wide Receiver", "Tight End"],
+        // correct: 3
+    },
+    {
+        question: "When will the Dallas Cowboys when their next Superbowl?",
+        choices: ["2017", "2018", "2060", "Never Again..."],
+        // correct: 1
+    },
+];
+
+// timer for the game -- 45 seconds
+var timer = 46;
+var intervalId;
+
+// makes timer count down at a rate of 1 second
+function run() {
+    intervalId = setInterval(decrement, 1000);
+};
+
+// function shows the timer counting down
+function decrement() {
+    timer--;
+    $('#timer').html('<h2>' + timer + '</h2>');
+    // makes game end at 0
+    if (timer === 0) {
+        alert('Game Over');
+        stop();
+    }
+};
+decrement();
+
+// stops the timer
+function stop() {
+    clearInterval(intervalId);
+}
+
+// display questions on html
+function displayQuestion() {
+
+    $.each(questions, function(key, element) {
+        $('#quiz').append('<p>' + element + '</p>' + '<br>');
+    });
+
+};
+
+displayQuestion();
+run();
+
+// answer bank of correct answers
+// answer bank of incorrect answers
+
+// verify if the answer is correct
+
 // display end of game screen with reults when time runs out
+
+
+
+
+
+
+
+// display answers as buttons on html
+// function displayAnswers () {
+// 	$.each(answers1, function(key, element){
+// 		$('#submit').append('<button>' + element + '</button>');
+// 	});
+// 	$.each(answers2, function(key, element){
+// 		$('#submit').append('<button>' + element + '</button>');
+// 	});
+// 	$.each(answers3, function(key, element){
+// 		$('#submit').append('<button>' + element + '</button>');
+// 	});
+// 	$.each(answers4, function(key, element){
+// 		$('#submit').append('<button>' + element + '</button>');
+// 	});
+// 	$.each(answers5, function(key, element){
+// 		$('#submit').append('<button>' + element + '</button>');
+// 	});
+// 	$.each(answers6, function(key, element){
+// 		$('#submit').append('<button>' + element + '</button>');
+// 	});
+// 	$.each(answers7, function(key, element){
+// 		$('#submit').append('<button>' + element + '</button>');
+// 	});
+// 	$.each(answers8, function(key, element){
+// 		$('#submit').append('<button>' + element + '</button>');
+// 	});
+// 	$.each(answers9, function(key, element){
+// 		$('#submit').append('<button>' + element + '</button>');
+// 	});
+// 	$.each(answers10, function(key, element){
+// 		$('#submit').append('<button>' + element + '</button>');
+// 	});
+// };
+// displayAnswers ();
+
+
