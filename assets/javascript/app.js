3a666c642d37e982d3acfdc986ff623d1e3e7ca7
Sam Sadler <samsadler9@sams-air.attlocal.net>
2017-08-27 15:14:50 -0500

    clicks are now tracking, but doesnt work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,63 +10,80 @@ var selections = [];
 // 10 questions for user to answer
 var questions = [{
         question: "Which player was not part of the Dallas Cowboys Triplets?",
-        choices: [" Emmitt Smith ", " Troy Aikman ", " Tony Dorsett ", " Michael Irvin"]
-        // correct: 2,
+        choices: [" Emmitt Smith ", " Troy Aikman ", " Tony Dorsett ", " Michael Irvin"],
+        correctAnswer: 2
     },
     {
         question: "When was the last time the Dallas Cowboys won The Superbowl?",
         choices: [" 1999 ", " 1996 ", " 1994 ", " 1997 "],
-        // correct: 1,
+        correctAnswer: 1
     },
     {
         question: "Where did Dak Prescott attend college?",
         choices: [" Mississippi State ", " Purdue ", " LSU ", " USC "],
-        // correct: 0
+        correctAnswer: 0
     },
     {
         question: "Roger Staubach played which position?",
         choices: [" Running Back ", " Wide Receiver ", " Left Out ", " Quarterback "],
-        // correct: 3
+        correctAnswer: 3
     },
     {
         question: "What record did Emmitt Smith set?",
         choices: [" Career Rushing Yards ", " Most Fumbles ", " Fastest 40 ", " Most Rushing Attempts "],
-        // correct: 0
+        correctAnswer: 0
     },
     {
         question: "Which team is the Dallas Cowboys mosted hated rival?",
         choices: [" New York Giants ", " Washington Redskins ", " Philadelphia Eagles ", " All of the Above "],
-        // correct: 3
+        correctAnswer: 3
     },
     {
         question: "Before the NFC/AFC Split, which team shared Dallas, TX?",
         choices: [" Tennessee Titans ", " Arizona Cardinals ", " Kansas City Chiefs ", " Houston Oilers "],
-        // correct: 2
+        correctAnswer: 2
     },
     {
         question: "What franchise is the most valued franchise in all of sports?",
         choices: [" New York Yankees ", " Dallas Cowboys ", " Miami Heat ", " Oakland Raiders "],
-        // correct: 1
+        correctAnswer: 1
     },
     {
         question: "What position did Jay Novacek play?",
         choices: [" Quarterback ", " Center ", " Wide Receiver ", " Tight End "],
-        // correct: 3
+        correctAnswer: 3
     },
     {
         question: "When will the Dallas Cowboys when their next Superbowl?",
         choices: [" 2017 ", " 2018 ", " 2060 ", " Never Again... "],
-        // correct: 1
+        correctAnswer: 1
     },
 ];
 
-// display questions on html
+// trackers the user clicks
+var userGuess = $('#quiz').click(function (){
+			// console.log(userGuess);
+			 // need to verify if userGuess is = correctAnswer
+			if (userGuess === CorrectAnswer){
+				correctAnswers++;
+				console.log(correctAnswers);
+			}
+		});
+			
+
+
 function displayQuestion(index, value) {
 	$.each(questions, function (index, value){
+		// displays questions to the quiz div box on html
 		$('#quiz').append('<p>' + value.question + '</p>' + '<br>');
+		// displays the answer choices to the quiz div box on html
 		$('#quiz').append('<button>' + value.choices[0] + '</button>' + '<button>' + value.choices[1] + '</button>' + '<button>' + value.choices[2] + '</button>' + '<button>' + value.choices[3] + '</button>' + '<br>' + '<br>');
+		// check if user choice is the correct choice
+		// then increment correctAnswers
+		// else if user choice is incorrect then increment incorrectAnswers
 	});
 };
+
 
 // timer for the game -- 45 seconds
 var timer = 46;
